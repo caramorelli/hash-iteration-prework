a7ab7d3fa44df42af374c4401aa0bd11243433bc
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 02:00:55 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,17 +9,6 @@
 def select_winner(passengers)
   winner = nil
   passengers.each do |key, val|
-    puts val if key.to_s.chars.last == 'a' && val.chars.first == 'A'
-    # puts val
+    val if key.to_s.chars.last == 'a' && val.chars.first == 'A'
   end 
 end
-
-passengers = {
-      suite_a: "Amanda Presley", 
-      suite_b: "Seymour Hoffman", 
-      suite_c: "Alfred Tennyson", 
-      suite_d: "Charlie Chaplin", 
-      suite_e: "Crumpet the Elf"
-      }
-
-select_winner(passengers)
